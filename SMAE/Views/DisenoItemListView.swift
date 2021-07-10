f53fedff7f4f760d2f144e30700ee470fff597eb
Luis Humberto <beto020017@gmail.com>
2021-07-10 01:18:41 -0500

    Patrón MVVM

--- SMAE/Views/DisenoItemListView.swift
+++ SMAE/Views/DisenoItemListView.swift
@@ -24,61 +24,7 @@ struct DisenoItemListView: View {
                         
                 }.ignoresSafeArea()
                 HStack{
-                    VStack(spacing: 0){
-                        Text("Cantidad sugerida")
-                            .modifier(disenoDatos(colorletra: "azulOscuro", colorFondo: "azulClaro", radio: 20))
-                            
-                        Text("\(datos.kcal) pieza")
-                            .modifier(disenoDatos(colorletra: "blanco", colorFondo: "azulIntermedio", radio: 10))
-                    }
-                    Spacer()
-                    VStack(spacing: 0){
-                        Text("Peso")
-                            .modifier(disenoDatos(colorletra: "azulOscuro", colorFondo: "azulClaro", radio: 20))
-                        
-                        Text("\(datos.kcal)")
-                            .modifier(disenoDatos(colorletra: "blanco", colorFondo: "azulIntermedio", radio: 10))
-                    }
-                }.padding()
-                HStack{
-                    VStack(spacing: 0){
-                        Text("Energia")
-                            .modifier(disenoDatos(colorletra: "azulOscuro", colorFondo: "amarillo", radio: 20))
- 
-                        Text("\(datos.kcal) kcal")
-                            .modifier(disenoDatos(colorletra: "blanco", colorFondo: "amarilloOscuro", radio: 10))
-                    }
-                    Spacer()
-                    VStack(spacing: 0){
-                        Text("Proteína")
-                            .modifier(disenoDatos(colorletra: "azulOscuro", colorFondo: "amarillo", radio: 20))
-                        
-                        Text("\(datos.proteinas) g")
-                            .modifier(disenoDatos(colorletra: "blanco", colorFondo: "amarilloOscuro", radio: 10))
-                    }
-                }.padding()
-                HStack{
-                    VStack(spacing: 0){
-                        Text("Lipidos")
-                            .modifier(disenoDatos(colorletra: "azulOscuro", colorFondo: "amarillo", radio: 20))
-                        
-                        Text("\(datos.kcal) g")
-                            .modifier(disenoDatos(colorletra: "blanco", colorFondo: "amarilloOscuro", radio: 10))
-                    }
-                    Spacer()
-                    VStack(spacing: 0){
-                        Text("Hidratos de carbono")
-                            .modifier(disenoDatos(colorletra: "azulOscuro", colorFondo: "amarillo", radio: 20))
-                        
-                        Text("\(datos.kcal) g")
-                            .modifier(disenoDatos(colorletra: "blanco", colorFondo: "amarilloOscuro", radio: 10))
-                    }
-                }.padding()
-                VStack(spacing: 0){
-                    Text("Micronutrimento")
-                        .modifier(disenoDatos(colorletra: "azulOscuro", colorFondo: "blanco", radio: 20))
-                    Text("\(datos.carbohidratos) mg")
-                        .modifier(disenoDatos(colorletra: "blanco", colorFondo: "gris", radio: 10))
+                    fondo()
                 }
         }.navigationTitle(datos.nombreElelemto)
         .background(Color("azulOscuro"))
@@ -92,3 +38,64 @@ struct DisenoItemListView_Previews: PreviewProvider {
         DisenoItemListView(datos: alimentos[0].elementos[0])
     }
 }
+
+/*
+ Spacer()
+ VStack(spacing: 0){
+     Text("Cantidad sugerida")
+         .modifier(disenoDatos(colorletra: "azulOscuro", colorFondo: "azulClaro", radio: 20))
+         
+     Text("\(datos.kcal) pieza")
+         .modifier(disenoDatos(colorletra: "blanco", colorFondo: "azulIntermedio", radio: 10))
+ }
+ Spacer()
+ VStack(spacing: 0){
+     Text("Peso")
+         .modifier(disenoDatos(colorletra: "azulOscuro", colorFondo: "azulClaro", radio: 20))
+     
+     Text("\(datos.kcal)")
+         .modifier(disenoDatos(colorletra: "blanco", colorFondo: "azulIntermedio", radio: 10))
+ }
+ Spacer()
+}.padding()
+HStack{
+ VStack(spacing: 0){
+     Text("Energia")
+         .modifier(disenoDatos(colorletra: "azulOscuro", colorFondo: "amarillo", radio: 20))
+
+     Text("\(datos.kcal) kcal")
+         .modifier(disenoDatos(colorletra: "blanco", colorFondo: "amarilloOscuro", radio: 10))
+ }
+ Spacer()
+ VStack(spacing: 0){
+     Text("Proteína")
+         .modifier(disenoDatos(colorletra: "azulOscuro", colorFondo: "amarillo", radio: 20))
+     
+     Text("\(datos.proteinas) g")
+         .modifier(disenoDatos(colorletra: "blanco", colorFondo: "amarilloOscuro", radio: 10))
+ }
+}.padding()
+HStack{
+ VStack(spacing: 0){
+     Text("Lipidos")
+         .modifier(disenoDatos(colorletra: "azulOscuro", colorFondo: "amarillo", radio: 20))
+     
+     Text("\(datos.kcal) g")
+         .modifier(disenoDatos(colorletra: "blanco", colorFondo: "amarilloOscuro", radio: 10))
+ }
+ Spacer()
+ VStack(spacing: 0){
+     Text("Hidratos de carbono")
+         .modifier(disenoDatos(colorletra: "azulOscuro", colorFondo: "amarillo", radio: 20))
+     
+     Text("\(datos.kcal) g")
+         .modifier(disenoDatos(colorletra: "blanco", colorFondo: "amarilloOscuro", radio: 10))
+ }
+}.padding()
+VStack(spacing: 0){
+ Text("Micronutrimento")
+     .modifier(disenoDatos(colorletra: "azulOscuro", colorFondo: "blanco", radio: 20))
+ Text("\(datos.carbohidratos) mg")
+     .modifier(disenoDatos(colorletra: "blanco", colorFondo: "gris", radio: 10))
+}
+ */
--- SMAE/Views/fondo.swift
+++ SMAE/Views/fondo.swift
@@ -9,7 +9,61 @@ import SwiftUI
 
 struct fondo: View {
     var body: some View {
-        Text("hello")
+        VStack {
+            HStack{
+                VStack{
+                    VStack(spacing: 0){
+                        Text("Cantidad sugerida")
+                            .modifier(disenoDatos(colorletra: "azulOscuro", colorFondo: "azulClaro", radio: 20))
+                        Text("dsfs) pieza")
+                            .modifier(disenoDatos(colorletra: "blanco", colorFondo: "azulIntermedio", radio: 10))
+                    }.padding(.bottom)
+                    VStack(spacing: 0){
+                        Text("Energia")
+                            .modifier(disenoDatos(colorletra: "azulOscuro", colorFondo: "amarillo", radio: 20))
+
+                        Text("12 kcal")
+                            .modifier(disenoDatos(colorletra: "blanco", colorFondo: "amarilloOscuro", radio: 10))
+                    }.padding(.bottom)
+                    VStack(spacing: 0){
+                        Text("Lipidos")
+                            .modifier(disenoDatos(colorletra: "azulOscuro", colorFondo: "amarillo", radio: 20))
+                        
+                        Text("dfs g")
+                            .modifier(disenoDatos(colorletra: "blanco", colorFondo: "amarilloOscuro", radio: 10))
+                    }
+                }
+                Spacer()
+                VStack{
+                    VStack(spacing: 0){
+                        Text("Peso")
+                            .modifier(disenoDatos(colorletra: "azulOscuro", colorFondo: "azulClaro", radio: 20))
+                        Text("29")
+                            .modifier(disenoDatos(colorletra: "blanco", colorFondo: "azulIntermedio", radio: 10))
+                    }.padding(.bottom)
+                    VStack(spacing: 0){
+                        Text("Energia")
+                            .modifier(disenoDatos(colorletra: "azulOscuro", colorFondo: "amarillo", radio: 20))
+
+                        Text("12 kcal")
+                            .modifier(disenoDatos(colorletra: "blanco", colorFondo: "amarilloOscuro", radio: 10))
+                    }.padding(.bottom)
+                    VStack(spacing: 0){
+                        Text("Hidratos de carbono")
+                            .modifier(disenoDatos(colorletra: "azulOscuro", colorFondo: "amarillo", radio: 20))
+                        
+                        Text("(da g")
+                            .modifier(disenoDatos(colorletra: "blanco", colorFondo: "amarilloOscuro", radio: 10))
+                    }
+                }
+            }.padding(40)
+            VStack(spacing: 0){
+             Text("Micronutrimento")
+                 .modifier(disenoDatos(colorletra: "azulOscuro", colorFondo: "blanco", radio: 20))
+             Text("dsfs mg")
+                 .modifier(disenoDatos(colorletra: "blanco", colorFondo: "gris", radio: 10))
+            }
+        }
     }
 }
 
